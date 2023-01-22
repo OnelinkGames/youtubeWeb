bcd5dda4888b4b93a214bb83b253d4f03ca0a69c
Andrius <ualamy@gmail.com>
2023-01-22 12:36:07 -0300

    Função createScheduleList do component Schedule List

--- schedule-list2/src/shared/components/schedule-list.tsx
+++ schedule-list2/src/shared/components/schedule-list.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // Interface for the list of mocked data.
 export interface List {
@@ -28,20 +28,68 @@ function sortList(firstDate: List, lastDate: List) {
     return 0;
 }
 
+// Function to compare date and return true or false
+function compareDate(lastDate: Date, currentDate: Date): boolean {
+    let yearCondition: boolean = lastDate.getFullYear() === currentDate.getFullYear();
+    let monthCondition: boolean = lastDate.getMonth() == currentDate.getMonth();
+    let dayCondition: boolean = lastDate.getDate() === currentDate.getDate();
+
+    if (yearCondition && monthCondition && dayCondition) {
+        return true
+    }
+
+    return false;
+}
+
+// Function to create the final ScheduleList that will be used by the component.
+function createScheduleList(list: Array<List>): Array<Array<List>> {
+    let newArray: Array<Array<List>> = [];
+    let tempArray: Array<List> = [];
+
+    list.forEach((element, index) => {
+        if (index != 0) {
+            let checkDate = compareDate(list[index -1].date, list[index].date);
+
+            if (checkDate) {
+                tempArray.push(element);
+            } else {
+                newArray.push(tempArray);
+                tempArray = [];
+                tempArray.push(element);
+            }
+        } else {
+            tempArray.push(element);
+        }
+
+        if (index === list.length - 1) {
+            newArray.push(tempArray);
+        }
+    })
+
+    return newArray;
+}
+
 function ScheduleList(props: ListProps) {
     // Recovering the props according to the interface.
     const { list } = props;
+
+    // UseState that will be used by the application.
+    const [scheduleList, setScheduleList] = useState(Array<Array<List>>)
 
     // UseEffect used to check the data.
     useEffect(() => {
         // Sorting the data to adjust it accordingly to the date
         let sortedList = list.sort(sortList)
 
-        console.log(sortedList);
+        // Create the ScheduleList that will be used by the component.
+        setScheduleList(createScheduleList(sortedList))
     }, [])
 
     return (
+        <>
         <div>{JSON.stringify(list)}</div>
+        <div>{JSON.stringify(scheduleList)}</div>
+        </>
     )
 }
 
